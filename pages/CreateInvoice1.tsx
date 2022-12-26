8329af7384a42f46ea6208c0b5c521873a8b37be
DimasNuryadin <dimasnuryadinn@gmail.com>
2022-12-26 16:38:47 +0700

    Create Components

---
+++ pages/CreateInvoice1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateInvoice1() {
+  return (
+    <div>CreateInvoice1</div>
+  )
+}
---
+++ pages/CreateInvoice2.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateInvoice2() {
+  return (
+    <div>CreateInvoice2</div>
+  )
+}
