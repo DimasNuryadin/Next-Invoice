ec144aac54f984d98f1978c1483c0f4cde6ab578
DimasNuryadin <dimasnuryadinn@gmail.com>
2023-02-24 18:20:57 +0700

    make footer hompage component

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           </div>
         </nav>
 
-        <section className="hero-banner">
+        <section className="hero-banner mb-4">
           <h1 className="title mb-3">TENTANG KAMI</h1>
           <p className="description">Berawal dengan kegiatan usaha yang hanya meliputi pekerjaan pengadaan barang dan jasa ketika didirikan pada tahun 2005 PT Kamojang Mandiri untuk kepentingan perusahaan di sekitar Area Kamojang, saat ini memperluas cakupan pelayanan barang dan jasa ke wilayah lainnya. Langkah-langkah antisipatif dan inovatif pun semakin mudah diwujudkan. PT Kamojang Mandiri tetap berdiri selama lebih satu dasa warsa menghadapi berbagai krisis, baik skala nasional maupun global. Pada tahun 2022, KM resmi menetapkan Visi dan Misi perusahaan untuk menjawab tantangan masa depan Perseroan. PT Kamojang Mandiri berkomitmen untuk melaksanakan peran pentingnya dalam menghadirkan kualitas kehidupan yang lebih baik. Diversifikasi bisnis menjadi keunggulan KM untuk menangkap potensi yang ada di pasar nasional dan internasional berupa pengembangan usaha ke bidang jasa pariwisata dan travel haji & umroh.</p>
         </section>
@@ -37,36 +37,53 @@ export default function Home() {
         <section style={{ display: 'block' }}>
           <div className="footer">
             <div className="gap"></div>
-
             <div className="email-form">
-              <p>Send Us a message</p>
-              <div className="input-email">
+              <p className="title mb-4">Send Us a message</p>
+              <div className="input-email mb-4 pt-1 pb-2">
                 <input
                   type="text"
                   placeholder="Full Name"
                   required
                 />
               </div>
-              <div className="input-email">
+              <div className="input-email mb-4 pb-2">
                 <input
                   type="text"
                   placeholder="Phone Number"
                   required
                 />
               </div>
-              <div className="input-email">
+              <div className="input-email mb-4 pb-2">
                 <input
                   type="email"
                   placeholder="Email"
                   required
                 />
               </div>
+              <textarea className="input-message mb-2" name="message" placeholder="Message" rows={6} />
 
-              <Button buttonType="btn-primary" label="Send" />
+              <div className="float-end">
+                <Button buttonType="btn-primary" label="Send" />
+              </div>
             </div>
 
             <div className="contact">
-              <p>item 2</p>
+              <h2 className="title mb-5">Contact Us</h2>
+
+              <div className="d-flex flex-row mb-4">
+                <Image src="/icon/phone-light.svg" width={28} height={28} alt="Phone"></Image>
+                <p className="ms-3">0812-2025-9288</p>
+              </div>
+
+              <div className="d-flex flex-row mb-4">
+                <Image src="/icon/email-light.svg" width={28} height={28} alt="Phone"></Image>
+                <p className="ms-3">kamojangmandiri1@gmail.com</p>
+              </div>
+
+              <div className="d-flex flex-row ">
+                <Image src="/icon/address-light.svg" width={28} height={28} alt="Phone"></Image>
+                <p className="ms-3">Jl. Raya Kamojang No. 74-Kab. Bandung</p>
+              </div>
             </div>
           </div>
         </section>
